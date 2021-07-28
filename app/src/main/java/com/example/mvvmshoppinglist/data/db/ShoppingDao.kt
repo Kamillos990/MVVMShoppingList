defcae68ca922f8e2414ffddf9d4d5b9be8a2646
Kamillos990 <kamilzombek99@gmail.com>
2021-07-28 12:44:13 +0200

    ShoppingItem adding implemented

---
+++ app/src/main/java/com/example/mvvmshoppinglist/data/db/ShoppingDao.kt
@@ -0,0 +1,21 @@
+package com.example.mvvmshoppinglist.data.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.example.mvvmshoppinglist.data.db.entities.ShoppingItem
+
+@Dao
+interface ShoppingDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(item : ShoppingItem)
+
+    @Delete
+    suspend fun delete(item : ShoppingItem)
+
+    @Query("DELETE FROM shopping_items")
+    fun deleteAllShoppingItems()
+
+    @Query("SELECT * FROM shopping_items")
+    fun getAllShoppingItems() : LiveData<List<ShoppingItem>>
+}
